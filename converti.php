c5c5a2591ee4016e8345ac1a45e570d0e1e40d3a
iregon <0iregon0@gmail.com>
2018-03-06 12:46:52 +0100

    controllo se il valore è un numero

--- converti.php
+++ converti.php
@@ -1,6 +1,12 @@
 <?php
     $km = $_POST["txt_km"];
 
-    echo "<h1>Chilometri: " . $km . "</h1>";
-    echo "<h2>Miglia: " . ($km * 0.621371) . "</h1>";
+    if(is_numeric($km)) {
+        echo "<h1>Chilometri: " . $km . "</h1>";
+        echo "<h2>Miglia: " . ($km * 0.621371) . "</h1>";
+    }
+    else {
+        echo "Valore on valido";
+    }
+    
 ?>
